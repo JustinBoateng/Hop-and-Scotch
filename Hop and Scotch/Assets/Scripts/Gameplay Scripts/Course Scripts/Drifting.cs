7277b096029ddbd715ae50c2ee355b9b66f28dfe
JustinBoateng <JustinBoateng@yahoo.com>
2022-08-02 08:20:06 -0400

    Background Graphics Progress DONE
    Added a background
    Added Rain Animation
    Had the clouds move
    Created a moon
    Implemented Parallax Background Movement

---
+++ Hop and Scotch/Assets/Scripts/Gameplay Scripts/Course Scripts/Drifting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drifting : MonoBehaviour
+{
+    Rigidbody2D MyRD;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyRD = GetComponent<Rigidbody2D>();
+        MyRD.gravityScale = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MyRD.velocity = new Vector2(0.5f, 0);
+    }
+}
---
+++ Hop and Scotch/Assets/Scripts/Gameplay Scripts/Course Scripts/ParallaxBackground.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+
+    public Transform cameraTransform; //there can be multiple players, so setting this up manually is necessary
+
+    private Vector3 lastCameraPosition;
+
+    private Vector3 deltaMovement;
+
+    [SerializeField] private Vector2 parallaxEffectMultiplier ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastCameraPosition = cameraTransform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        deltaMovement = cameraTransform.position - lastCameraPosition; //this tracks how much the camera has moved since the previous frame. 
+                                                                       //LastUpdate makes sure this happens AFTER the camera has moved
+
+        transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y, 0);    //we move the background piece this script is attached to by the camera movement
+
+        lastCameraPosition = cameraTransform.position;   //then we reset our lastcameraposition to our currentcamera position
+    }
+}
